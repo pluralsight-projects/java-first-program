69bb175cc32590bf159b8894b8567343f2073306
narmadharavi <narmadha826@gmail.com>
2021-01-04 18:02:28 +0000

    module 2 tasks

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -4,7 +4,7 @@ package com.h2;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
@@ -12,6 +12,14 @@ public class App
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return 2 * number;
+    }
+
+    private static int add(int[] numbers) {
+        var sum = 0;
+        for(int number: numbers) {
+            sum += number;
+        }
+        return sum;
     }
 }
