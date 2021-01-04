d8381f37a4d03ec45506faaffe4331e3cd924581
narmadharavi <narmadha826@gmail.com>
2021-01-04 18:10:55 +0000

    Update BestLoanRates.java

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -3,7 +3,11 @@ import java.util.Scanner;
 import java.util.Map;
 
 public class BestLoanRates {
-    public static final Map<Integer,Float> bestRates = Map.of(1,5.50f,2,3.45f,3,2.67f);
+    public static final Map<Integer, Float> bestRates = Map.of(
+            1, 5.50f,
+            2, 3.45f,
+            3, 2.67f
+    );
     public static void main(String[] args){
         Scanner scanner= new Scanner(System.in);
         System.out.println("Enter your name");
@@ -13,13 +17,15 @@ public class BestLoanRates {
         int loanTermInYears = scanner.nextInt();
         float bestRate = getRates(loanTermInYears);
         if (bestRate == 0.0f){
-            System.out.println("No available rates for term: " + loanTermInYears + "years" );
-        }else{
-            System.out.println("Best Available Rates: " + getRates(loanTermInYears) + "%");
+            System.out.println("No available rates for term: " + loanTermInYears + " years" );
+        } else {
+            System.out.println("Best Available Rate: " + getRates(loanTermInYears) + "%");
         }
         scanner.close();
     }
     public static float getRates(int loanTermInYears){
-        return 0.0f;
-    }
+        if(bestRates.containsKey(loanTermInYears)) {
+            return bestRates.get(loanTermInYears);
+        }
+        return 0.0f;    }
 }
